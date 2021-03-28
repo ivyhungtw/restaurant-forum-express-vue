ab6e0441a693c19bd8329bd602d93467f9fec5b0
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-03-28 11:05:20 -0700

    Add isAdmin attribute to user model and table

--- models/user.js
+++ models/user.js
@@ -15,7 +15,8 @@ module.exports = (sequelize, DataTypes) => {
     {
       name: DataTypes.STRING,
       email: DataTypes.STRING,
-      password: DataTypes.STRING
+      password: DataTypes.STRING,
+      isAdmin: DataTypes.BOOLEAN
     },
     {
       sequelize,
