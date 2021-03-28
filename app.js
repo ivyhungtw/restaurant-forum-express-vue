35da9212530098bb74bfbbac1f5111190fdff5cb
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-03-28 09:52:46 -0700

    Confirm signup form information

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(flash())
 app.use((req, res, next) => {
   res.locals.successMsg = req.flash('successMsg')
   res.locals.errorMsg = req.flash('errorMsg')
+  res.locals.warningMsg = req.flash('warningMsg')
   next()
 })
 
--- controllers/userController.js
+++ controllers/userController.js
@@ -8,12 +8,61 @@ const userController = {
   },
 
   signUp: async (req, res) => {
-    await User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
-    })
-    return res.redirect('/signin')
+    const { name, email, password, confirmPassword } = req.body
+    const emailRule = /^\w+((-\w+)|(\.\w+)|(\+\w+))*@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z]+$/
+    const errors = []
+    // Before creating an account,
+    // make sure all the required fields are correct
+    if (!name || !email || !password || !confirmPassword) {
+      errors.push({ message: 'Please fill out all fields.' })
+    }
+    if (email.search(emailRule) === -1) {
+      errors.push({ message: 'Please enter the correct email address.' })
+    }
+    if (password !== confirmPassword) {
+      errors.push({ message: 'Password and confirmPassword do not match.' })
+    }
+    if (errors.length > 0) {
+      return res.render('signup', {
+        name,
+        email,
+        password,
+        confirmPassword,
+        errors
+      })
+    }
+
+    try {
+      // make sure email has not been used yet
+      const user = await User.findOne({ where: { email } })
+
+      if (user) {
+        req.flash(
+          'warningMsg',
+          `A user with ${email} already exists. Choose a different address or login directly.`
+        )
+        return res.redirect('/signup')
+      }
+
+      await User.create({
+        name,
+        email,
+        password: bcrypt.hashSync(
+          req.body.password,
+          bcrypt.genSaltSync(10),
+          null
+        )
+      })
+
+      req.flash(
+        'successMsg',
+        `${req.body.email} register successfully! Please login.`
+      )
+
+      return res.redirect('/signin')
+    } catch (error) {
+      console.log(error)
+    }
   }
 }
 
