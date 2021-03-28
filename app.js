42908d698828b078be2825a3369e7342ab43270d
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-03-28 09:26:19 -0700

    Istall express-session and connect-flash

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
 
 require('./models')
 
@@ -15,6 +17,14 @@ app.engine(
 )
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
+
+app.use((req, res, next) => {
+  res.locals.successMsg = req.flash('successMsg')
+  res.locals.errorMsg = req.flash('errorMsg')
+  next()
+})
 
 app.listen(port, () => {
   console.log(`App is listening at http://localhost:${port}`)
