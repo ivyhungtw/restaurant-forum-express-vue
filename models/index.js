2d8a6aea9d336aeaf0fd88bb6e263c91b1e50fc8
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-03-28 08:53:43 -0700

    Create user model

--- models/index.js
+++ models/index.js
@@ -13,17 +13,26 @@ let sequelize
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config)
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config)
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  )
 }
 
 // 動態引入其他 models
-fs
-  .readdirSync(__dirname)
+fs.readdirSync(__dirname)
   .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js')
+    return (
+      file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
+    )
   })
   .forEach(file => {
-    const model = sequelize['import'](path.join(__dirname, file))
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes
+    )
     db[model.name] = model
   })
 
