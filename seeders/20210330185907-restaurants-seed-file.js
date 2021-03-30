810f680da1df7a74915345c322cf9844f24e53cf
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-03-30 12:04:24 -0700

    Install faker and add restaurant seed data

---
+++ seeders/20210330185907-restaurants-seed-file.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const faker = require('faker')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert(
+      'Restaurants',
+      Array.from({ length: 50 }).map((d, i) => ({
+        name: faker.name.findName(),
+        tel: faker.phone.phoneNumber(),
+        address: faker.address.streetAddress(),
+        opening_hours: '08:00',
+        image: `https://loremflickr.com/320/240/restaurant,food/?random=${
+          Math.random() * 100
+        }`,
+        description: faker.lorem.text(),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      })),
+      {}
+    )
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Restaurants', null, {})
+  }
+}
