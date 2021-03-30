e09bdf7c56bb6e16c205539ccb118fff6e0c07c5
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-03-30 11:57:15 -0700

    Add user seed data

---
+++ seeders/20210330185304-users-seed-file.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const bcrypt = require('bcryptjs')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert(
+      'Users',
+      [
+        {
+          email: 'root@example.com',
+          password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+          isAdmin: true,
+          name: 'root',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          email: 'user1@example.com',
+          password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+          isAdmin: false,
+          name: 'user1',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          email: 'user2@example.com',
+          password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+          isAdmin: false,
+          name: 'user2',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ],
+      {}
+    )
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Users', null, {})
+  }
+}
