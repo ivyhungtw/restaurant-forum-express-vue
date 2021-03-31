ae56e3c3c0aa6ed56d9d86719725105bba88dc81
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-03-31 10:07:00 -0700

    Install Imgur api and update admin controller

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,6 +2,8 @@ const db = require('../models')
 const Restaurant = db.Restaurant
 
 const fs = require('fs').promises
+const imgur = require('imgur-node-api')
+const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 
 const adminController = {
   getRestaurants: async (req, res) => {
@@ -27,28 +29,59 @@ const adminController = {
       })
     }
 
-    try {
-      // Files stored in /temp through multer middleware will be removed in the future,
-      // so I need to keep the successfully uploaded image in /upload
-      if (file) {
-        const data = await fs.readFile(file.path)
-        await fs.writeFile(`upload/${file.originalname}`, data)
-      }
-
-      await Restaurant.create({
+    // Files stored in /temp through multer middleware will be removed in the future,
+    // so I need to keep the successfully uploaded image by uploading to imgur through API
+    if (file) {
+      imgur.setClientID(IMGUR_CLIENT_ID)
+      imgur.upload(file.path, (err, img) => {
+        return Restaurant.create({
+          name,
+          tel,
+          address,
+          opening_hours,
+          description,
+          image: file ? img.data.link : null
+        }).then(restaurant => {
+          req.flash('successMsg', 'Restaurant was created successfully')
+          return res.redirect('/admin/restaurants')
+        })
+      })
+    } else {
+      return Restaurant.create({
         name,
         tel,
         address,
         opening_hours,
         description,
-        image: file ? `/upload/${file.originalname}` : null
+        image: null
+      }).then(restaurant => {
+        req.flash('successMsg', 'Restaurant was created successfully')
+        return res.redirect('/admin/restaurants')
       })
-
-      req.flash('successMsg', 'Restaurant was created successfully')
-      return res.redirect('/admin/restaurants')
-    } catch (err) {
-      console.log(err)
     }
+
+    // try {
+    //   // Files stored in /temp through multer middleware will be removed in the future,
+    //   // so I need to keep the successfully uploaded image in /upload
+    //   if (file) {
+    //     const data = await fs.readFile(file.path)
+    //     await fs.writeFile(`upload/${file.originalname}`, data)
+    //   }
+
+    //   await Restaurant.create({
+    //     name,
+    //     tel,
+    //     address,
+    //     opening_hours,
+    //     description,
+    //     image: file ? `/upload/${file.originalname}` : null
+    //   })
+
+    //   req.flash('successMsg', 'Restaurant was created successfully')
+    //   return res.redirect('/admin/restaurants')
+    // } catch (err) {
+    //   console.log(err)
+    // }
   },
   getRestaurant: async (req, res) => {
     const restaurant = await Restaurant.findByPk(req.params.id, { raw: true })
