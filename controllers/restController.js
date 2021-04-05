191c7c72de73234202dcb117928f242793e8efc2
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-04-05 11:12:27 -0700

    Add pagination on restaurants index page

--- controllers/restController.js
+++ controllers/restController.js
@@ -2,24 +2,42 @@ const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
 
+const pageLimit = 10
+
 const restController = {
   getRestaurants: async (req, res) => {
+    let offset = 0
     const whereQuery = {}
     let categoryId = ''
+
+    if (req.query.page) {
+      offset = (req.query.page - 1) * pageLimit
+    }
+
     if (req.query.categoryId) {
       categoryId = Number(req.query.categoryId)
       whereQuery.CategoryId = categoryId
     }
 
-    const [categories, restaurants] = await Promise.all([
+    const [categories, result] = await Promise.all([
       Category.findAll({ raw: true, nest: true }),
-      Restaurant.findAll({
+      Restaurant.findAndCountAll({
         include: Category,
-        where: whereQuery
+        where: whereQuery,
+        offset: offset,
+        limit: pageLimit
       })
     ])
 
-    const data = restaurants.map(restaurant => ({
+    // data for pagination
+    const page = Number(req.query.page) || 1
+    const pages = Math.ceil(result.count / pageLimit)
+    const totalPage = Array.from({ length: pages }).map((_, index) => index + 1)
+    const prev = page - 1 < 1 ? 1 : page - 1
+    const next = page + 1 > pages ? pages : page + 1
+
+    // clean up restaurant data
+    const data = result.rows.map(restaurant => ({
       ...restaurant.dataValues,
       description: restaurant.dataValues.description.substring(0, 50),
       categoryName: restaurant.Category.name
@@ -28,7 +46,11 @@ const restController = {
     return res.render('restaurants', {
       restaurants: data,
       categories,
-      categoryId
+      categoryId,
+      page,
+      totalPage,
+      prev,
+      next
     })
   },
   getRestaurant: async (req, res) => {
