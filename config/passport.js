590f53963eaf0307f19d87b9b71f3cf40c8285b8
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-04-12 11:27:38 -0700

    Add authenticated and authenticatedAdmin

--- config/passport.js
+++ config/passport.js
@@ -57,4 +57,28 @@ passport.deserializeUser(async (id, done) => {
   }
 })
 
+// JWT
+const jwt = require('jsonwebtoken')
+const passportJWT = require('passport-jwt')
+const ExtractJwt = passportJWT.ExtractJwt
+const JwtStrategy = passportJWT.Strategy
+
+const jwtOptions = {}
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken()
+jwtOptions.secretOrKey = process.env.JWT_SECRET
+
+const strategy = new JwtStrategy(jwtOptions, async (jwt_payload, next) => {
+  const user = await User.findByPk(jwt_payload.id, {
+    include: [
+      { model: db.Restaurant, as: 'FavoritedRestaurants' },
+      { model: db.Restaurant, as: 'LikedRestaurants' },
+      { model: User, as: 'Followers' },
+      { model: User, as: 'Followings' }
+    ]
+  })
+  if (!user) return next(null, false)
+  return next(null, user)
+})
+passport.use(strategy)
+
 module.exports = passport
--- routes/apis.js
+++ routes/apis.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
+const passport = require('../config/passport')
 const adminController = require('../controllers/api/adminController.js')
 const categoryController = require('../controllers/api/categoryController.js')
 const userController = require('../controllers/api/userController.js')
@@ -8,22 +9,39 @@ const userController = require('../controllers/api/userController.js')
 const multer = require('multer')
 const upload = multer({ dest: 'temp/' })
 
+const authenticated = passport.authenticate('jwt', { session: false })
+
+const authenticatedAdmin = (req, res, next) => {
+  if (req.user) {
+    if (req.user.isAdmin) {
+      return next()
+    }
+    return res.json({ status: 'error', message: 'permission denied' })
+  } else {
+    return res.json({ status: 'error', message: 'permission denied' })
+  }
+}
+
 router
   .route('/admin/restaurants')
+  .all(authenticated, authenticatedAdmin)
   .get(adminController.getRestaurants)
   .post(upload.single('image'), adminController.postRestaurant)
 router
   .route('/admin/restaurants/:id')
+  .all(authenticated, authenticatedAdmin)
   .get(adminController.getRestaurant)
   .put(upload.single('image'), adminController.putRestaurant)
   .delete(adminController.deleteRestaurant)
 
 router
   .route('/admin/categories')
+  .all(authenticated, authenticatedAdmin)
   .get(categoryController.getCategories)
   .post(categoryController.postCategories)
 router
   .route('/admin/categories/:id')
+  .all(authenticated, authenticatedAdmin)
   .get(categoryController.getCategories)
   .put(categoryController.putCategory)
   .delete(categoryController.deleteCategory)
