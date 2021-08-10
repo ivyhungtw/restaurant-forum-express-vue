f0868311a9fb30b5b74e8d7d1983dbc40a5c5ad4
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-08-09 21:24:51 -0500

    refactor: modify pages' ui

---
+++ dist/js/chunk-2d0e5e97.85e2a040.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e5e97"],{9703:function(n,e,t){"use strict";t.r(e);var c=t("7a23");function o(n,e){return Object(c["t"])(),Object(c["e"])("h1",null,"404 Not Found")}const u={};u.render=o;e["default"]=u}}]);
+//# sourceMappingURL=chunk-2d0e5e97.85e2a040.js.map
